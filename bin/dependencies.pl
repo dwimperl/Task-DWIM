dbf337807c88f4046abf7687462f714868e19843
Gabor Szabo <gabor@szabgab.com>
2012-12-14 14:16:47 +0200

    what to do with the dependencies

--- bin/dependencies.pl
+++ bin/dependencies.pl
@@ -17,5 +17,16 @@ foreach my $name (sort keys %modules) {
     my $module   = $mcpan->module( $name );
     my $dist     = $mcpan->release( distribution => $module->{distribution} );
     say "$module->{distribution}  $module->{version}";
-    die Dumper $dist;
+    foreach my $dependency (@{ $dist->{dependency} }) {
+       say "   $dependency->{module}  $dependency->{version}"
+    }
+    #print Dumper $dist;
 }
+
+# Convert the modules.txt to some other format (JSON or YAML ?)
+# list of module => version
+# list of module => distribution
+# in the end we actually install distributions, so we probably
+# need a list of those with version numbers and full path
+# we would also need a dependency mapping so we know the order of installation
+
